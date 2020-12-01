64f7af81c0ee3009b2ecd1796a951592ad26e37e
Didych Oleksandr <lilsanya044@icloud.com>
2020-12-01 21:03:23 +0200

    innitial commit

---
+++ telegram.php
@@ -0,0 +1,23 @@
+<?php
+
+$date = $_POST['date'];
+$time = $_POST['time'];
+$adress = $_POST['adress'];
+
+$token = "1390388368:AAHh7PrOtBHBa58PsLYkUVWYhEIKVlkMfdw";
+$chat_id = "-386083989";
+$arr = array ( 
+    
+    'ДАТА: ' => $name,
+    'ЧАС: ' => $time,
+    'АДРЕС: ' => $adress
+);
+
+foreach($arr as $key => $value) {
+    $txt .= "<b>".key."/b".$value. "%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}", "r");
+?>
+
+
